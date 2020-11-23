318477297643e0a056dffd2ff12d68b32a1d4545
Lokesh Garia <63031525+lokeshgaria@users.noreply.github.com>
2020-11-23 18:43:57 +0530

    added dynamic port for heroku

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ const path = require('path');
 const hbs = require('hbs')
 const App = express();
 
+//creating port 
+const Port = process.env.PORT || 8080;
 
 const static_path = path.join(__dirname , "../public" );
 const Template_path = path.join(__dirname , "../templates/views" );
@@ -25,6 +27,6 @@ App.get("/weather",(req , res )=>{
 App.get("*",(req , res )=>{
     res.render("404error");
 });
-App.listen(8080, ()=>{
+App.listen(Port, ()=>{
     console.log("listining to port 8080..");
 });
